e64d2c1666e134356bc8c342320c6be71ec83ded
Aingeru Sabando <AI_SA@ec4u.de>
2017-07-25 23:10:42 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var sLog = `${now}: ${req.method} ${req.url}`;
+  console.log(`${now}: ${req.method} ${req.url}`);
+  fs.appendFile('server.log', sLog +'\n', (err)=>{if(err)console.log('Unable to applet log to file');});
+  next();
+});
+
+//app.use((req, res, next)=>{
+//  res.render('maintenance.hbs');
+//});
+
+app.use(express.static(__dirname +"/public"));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (sText)=>{
+  return(sText.toUpperCase());
+});
+
+app.get('/',(req, res)=>{
+  //res.send('<html><head><title>Express tests</title></head><body><h1>Hello Express!</h1></body>');
+  res.render('home.hbs',{
+    webPageName: 'Home',
+    pageTitle: 'Home Express Page',
+    currentYear: new Date().getFullYear(),
+    welcomePageMsg: 'Welcome to my test web page!'
+  });
+});
+
+app.get('/about',(req, res)=>{
+  res.render('about.hbs',{
+    webPageName: 'Express About',
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req, res)=>{
+  res.send({errorMessage: 'Cannot handle this request'});
+});
+
+app.listen(3000, ()=> {
+  console.log('Server is up in port 3000.');
+});
